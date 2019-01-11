770afdc2c79801adc57cd1f66d6eceebc7ba1d22
Brent <brent@Brents-MacBook-Pro.local>
2019-01-11 11:40:18 -0500

    message

---
+++ 001 - 025/01-multiplesOf.js
@@ -0,0 +1,13 @@
+let y;
+
+for (let x = 0; x <= 1000; x++) {
+    switch (true){
+        case x % 3 == 0 || x % 5 == 0:
+        console.log(x);
+        y = x + y;
+        break;
+    default:
+}
+    }
+
+console.log(y);
